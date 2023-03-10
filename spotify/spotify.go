922a695c1b17dc2be23260e9e9c7267f4ce16416
Andrei Matei <andrei.matei@mongodb.com>
2023-03-10 00:23:02 +0000

    feat: add client logic

--- spotify/spotify.go
+++ spotify/spotify.go
@@ -1,1 +1,164 @@
 package spotify
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+const (
+	Version = "v0.0.0"
+
+	libraryName      = "go-spotify"
+	defaultBaseURL   = "https://api.spotify.com/v1/"
+	defaultUserAgent = libraryName + "/" + Version
+
+	mediaTypeJSON = "application/json"
+)
+
+type Client struct {
+	client *http.Client
+
+	BaseURL   *url.URL
+	UserAgent string
+
+	common service
+
+	// Services used to interact with different parts of the Spotify API
+}
+
+type service struct {
+	client *Client
+}
+
+func NewClient(httpClient *http.Client) *Client {
+	if httpClient == nil {
+		httpClient = &http.Client{}
+	}
+
+	baseUrl, _ := url.Parse(defaultBaseURL)
+
+	c := &Client{client: httpClient, BaseURL: baseUrl, UserAgent: defaultUserAgent}
+	c.common.client = c
+
+	// Initialize services
+
+	return c
+}
+
+func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Request, error) {
+	if !strings.HasSuffix(c.BaseURL.Path, "/") {
+		return nil, fmt.Errorf("BaseURL must have a trailing slash, but %q does not", c.BaseURL)
+	}
+
+	u, err := c.BaseURL.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf io.ReadWriter
+	if body != nil {
+		buf = &bytes.Buffer{}
+		enc := json.NewEncoder(buf)
+		enc.SetEscapeHTML(false)
+		err := enc.Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	if body != nil {
+		req.Header.Set("Content-Type", mediaTypeJSON)
+	}
+	req.Header.Add("Accept", mediaTypeJSON)
+	if c.UserAgent != "" {
+		req.Header.Set("User-Agent", c.UserAgent)
+	}
+
+	return req, nil
+}
+
+type Response struct {
+	Response *http.Response
+}
+
+func newResponse(r *http.Response) *Response {
+	response := &Response{Response: r}
+	return response
+}
+
+func (c *Client) Do(ctx context.Context, req *http.Request, v interface{}) (*Response, error) {
+	// TODO: Add support for rate limiting
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		// If we got an error, and the context has been canceled,
+		// the context's error is probably more useful.
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+		}
+
+		return nil, err
+	}
+
+	response := newResponse(resp)
+
+	err = CheckReponse(resp)
+	if err != nil {
+		defer resp.Body.Close()
+		return response, err
+	}
+
+	switch v := v.(type) {
+	case nil:
+	case io.Writer:
+		_, err = io.Copy(v, resp.Body)
+	default:
+		decErr := json.NewDecoder(resp.Body).Decode(v)
+		if decErr == io.EOF {
+			decErr = nil // ignore EOF errors caused by empty response body
+		}
+		if decErr != nil {
+			err = decErr
+		}
+	}
+	return response, err
+}
+
+type ErrorResponse struct {
+	Response *http.Response `json:"-"`       // HTTP response that cause this error
+	Status   int            `json:"status"`  // The HTTP status code
+	Message  string         `json:"message"` // A short description of the cause of the error.
+}
+
+func (r *ErrorResponse) Error() string {
+	return fmt.Sprintf("%v %v: %d %v",
+		r.Response.Request.Method, r.Response.Request.URL,
+		r.Response.StatusCode, r.Message)
+}
+
+func CheckReponse(r *http.Response) error {
+	if c := r.StatusCode; 200 <= c && c <= 299 {
+		return nil
+	}
+
+	errorResponse := &ErrorResponse{Response: r}
+	data, err := io.ReadAll(r.Body)
+	if err == nil && data != nil {
+		json.Unmarshal(data, errorResponse)
+	}
+
+	return errorResponse
+}
---
+++ spotify/spotify_test.go
@@ -0,0 +1,3 @@
+package spotify
+
+// TODO: Add test for client
